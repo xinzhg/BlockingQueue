1fd5bd08f0320c5a3d19e2656b4b8c04d0f95a02
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-05-07 14:27:39 -0700

    Add Enqueue method

--- BlockingQueue.go
+++ BlockingQueue.go
@@ -1,12 +1,26 @@
 package BlockingQueue
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 type BlockingQueue struct {
 	size int
 	cond *sync.Cond
+	data []int
 }
 
 func NewBlockingQueue(size int) *BlockingQueue {
-	return &BlockingQueue{size, sync.NewCond(&sync.Mutex{})}
+	return &BlockingQueue{size, sync.NewCond(&sync.Mutex{}), make([]int, 0, size)}
+}
+
+func (b *BlockingQueue) Enqueue(val int) error {
+	b.cond.L.Lock()
+	defer b.cond.L.Unlock()
+	if len(b.data) == b.size {
+		return errors.New("reach to the limit size")
+	}
+	b.data = append(b.data, val)
+	return nil
 }
