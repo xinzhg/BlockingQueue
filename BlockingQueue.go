a681a30fc803f58d0cae7959da455850df6d413f
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-05-07 14:09:09 -0700

    Add constructor of blocking queue

--- BlockingQueue.go
+++ BlockingQueue.go
@@ -4,5 +4,9 @@ import "sync"
 
 type BlockingQueue struct {
 	size int
-	cond sync.Cond
+	cond *sync.Cond
+}
+
+func NewBlockingQueue(size int) *BlockingQueue {
+	return &BlockingQueue{size, sync.NewCond(&sync.Mutex{})}
 }
